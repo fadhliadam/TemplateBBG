d1a911353feb2a192ca2599f194dbeefc73cb963
Muhxdan <danufirdaus51@gmail.com>
2023-03-17 09:35:32 +0700

    add list adapter

--- app/src/main/java/com/example/templetebbg/ui/adapter/ListAdapter.kt
+++ app/src/main/java/com/example/templetebbg/ui/adapter/ListAdapter.kt
@@ -1,4 +1,49 @@
 package com.example.templetebbg.ui.adapter
 
-class ListAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.templetebbg.databinding.ListItemBinding
+import com.example.templetebbg.domain.Entity
+
+class ListAdapter: RecyclerView.Adapter<ListAdapter.MovieViewHolder>() {
+    var onItemClick: ((Entity) -> Unit)? = null
+
+    private val differCallback = object : DiffUtil.ItemCallback<Entity>(){
+        override fun areItemsTheSame(oldItem: Entity, newItem: Entity): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Entity, newItem: Entity): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    inner class MovieViewHolder(val binding: ListItemBinding): RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.root.setOnClickListener {
+                onItemClick?.invoke(differ.currentList[adapterPosition])
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val itemsBinding = ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MovieViewHolder(itemsBinding)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val item = differ.currentList[position]
+        holder.binding.apply {
+            tvId.text = item.id.toString()
+            tvTitle.text = item.title
+            tvBody.text = item.body
+        }
+    }
+
+    override fun getItemCount(): Int =  differ.currentList.size
 }
--- app/src/main/java/com/example/templetebbg/ui/bottomnav/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/templetebbg/ui/bottomnav/ui/home/HomeFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.templetebbg.ui.bottomnav.databinding.FragmentHomeBinding
+import com.example.templetebbg.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
@@ -28,10 +28,6 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
