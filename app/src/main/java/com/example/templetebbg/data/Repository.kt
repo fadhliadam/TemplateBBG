3ebb5884c19011834a95b0fe5ec3d15d90dbf7ec
Esa Ibrahim <esa.ibrahim25@gmail.com>
2023-03-20 10:19:11 +0700

    Add App Module

--- app/src/main/java/com/example/templetebbg/data/Repository.kt
+++ app/src/main/java/com/example/templetebbg/data/Repository.kt
@@ -1,7 +1,11 @@
 package com.example.templetebbg.data
 
+import com.example.templetebbg.data.remote.ApiService
 import com.example.templetebbg.domain.Entity
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class Repository {
+@Singleton
+class Repository @Inject constructor(api: ApiService) {
 
 }
